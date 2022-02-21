511084b239a33dd040745b3587545c020e2526b7
Mathe Akos <matheakos2002@gmail.com>
2022-02-21 13:48:27 +0100

    3

---
+++ utikoltseg.js
@@ -0,0 +1,12 @@
+var nev = document.getElementById("nev");
+var lakcim = document.getElementById("lakcim");
+var szuldatum = document.getElementById("szuldatum");
+var gyartmany = document.getElementById("gyartmany");
+var uzemanyag = document.getElementById("uzemanyagtipus");
+var norma = document.getElementById("norma");
+var benzinar = ["1000 cm3-ig 7,6 l/100km", "1001-1500 cm3 között 8,6 l/100km", "1501-2000 cm3 között 9,5 l/100km", "2001-3000 cm3 között 11,4 l/100km", "3001 cm3 fölött 13,3 l/100km"];
+var dizelar = ["1500 cm3-ig 5,7 l/100km", "1501-2000 cm3 között 6,7 l/100km", "2001-3000 cm3 között 7,6 l/100km", "3001 cm3 fölött 9,5 l/100km"];
+
+function uzemanyagtipus(){
+    
+}
